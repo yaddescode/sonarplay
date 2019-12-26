54ed6761b0f750f79df2a5b5124004434980a39d
Yasha Addes <yasha.addes@nexttrucking.com>
2019-12-25 23:12:23 -0800

    sonarplay initial

---
+++ src/main/java/net/yasha/sonarplay/SonarplayApplication.java
@@ -0,0 +1,13 @@
+package net.yasha.sonarplay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarplayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarplayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/yasha/sonarplay/SonarplayApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yasha.sonarplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
